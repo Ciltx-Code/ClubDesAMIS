71b71239c42da1c15210e26608115d88c206d73c
Lucas <lucas.garlaschi@yahoo.com>
2022-05-06 08:58:39 +0200

    lala

--- src/Controller/PdfCreatorController.php
+++ src/Controller/PdfCreatorController.php
@@ -24,20 +24,44 @@ class PdfCreatorController extends AbstractController
             ->findAll();
         $fpdf = new Fpdf();
         $fpdf->AddPage();
-        $fpdf->SetFont('Arial','B',16);
-        foreach ($amis as $ami){
-            $fpdf->Cell(40,10,$ami->getNom(),1,2,'C');
+        $fpdf->SetFont('Arial', 'B', 16);
+
+        // Logo
+        $fpdf->Image('image.jpg', 10, 6, 30);
+        // Arial bold 15
+        $fpdf->SetFont('Arial', 'B', 15);
+        // Move to the right
+        $fpdf->Cell(71);
+        // Title
+        $fpdf->Cell(50, 10, 'Liste des AMIS', 1, 0, 'C');
+        // Line break
+        $fpdf->Ln(25);
+
+        $header = array(iconv("UTF-8", "windows-1252", 'NOM'), iconv("UTF-8", "windows-1252", 'PRÉNOM'),);
+        // Header
+        $fpdf->SetFont('Arial', 'B', 20);
+        foreach ($header as $col){
+            $fpdf->Cell(95, 12, $col, 1,0,"C");
+        }
+        $fpdf->Ln();
+        $fpdf->SetFont('Arial', '', 15);
+        foreach ($amis as $row) {
+            $fpdf->Cell(95, 8, iconv("UTF-8", "windows-1252", $row->getNom()), 1,0,"C");
+            $fpdf->Cell(95, 8, iconv("UTF-8", "windows-1252", $row->getPrenom()), 1,0,"C");
             $fpdf->Ln();
         }
-        $fpdf->Output('D');
+
+
+        $fpdf->Output('D',"LISTE_AMIS.pdf", true);
 
         $response = $this->render("pdf_creator/index.html.twig",
             array(
-                'pdf'=>$fpdf,
-                'controller_name'=>'mkengiuh'
+                'pdf' => $fpdf,
+                'controller_name' => 'pdf'
             ));
 
         $response->headers->set('Content-type', 'application/pdf');
         return $response;
     }
+
 }
